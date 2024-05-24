e9f4d1dd2067c7e7b070936d5f9f2da4f4235918
J3anP <a20213733@pucp.edu.pe>
2024-05-23 21:05:25 -0500

    ola5

--- src/main/java/org/example/labgticsz/controller/HomeController.java
+++ src/main/java/org/example/labgticsz/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.example.labgticsz.repository.UserRepository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype   .Controller;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
